1d8f428974457dd1a60a231232e2d7c33cc61cd2
trung135 <trungls135@gmail.com>
2024-11-04 21:23:51 +0700

    Update Code

--- Assets/Managed/SpriteSheetVault.cs
+++ Assets/Managed/SpriteSheetVault.cs
@@ -1,13 +1,29 @@
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 public class SpriteSheetVault : MonoBehaviour
 {
-    public void Start()
-    {
-    }
-
-    public void Update()
-    {
-    }
+    // [SerializeField]
+    // private SpriteSheetAsset[] spriteSheetAssets;
+    // public void Awake()
+    // {
+    //     var spritesheets = spriteSheetAssets.AsSpan();
+    //     foreach (var spritesheet in spritesheets)
+    //     {
+    //         if (!spritesheet) continue;
+    //
+    //         var assetId = spritesheet.Id;
+    //         var sheets = spritesheet.SpriteSheets.Span;
+    //
+    //         foreach (var sheet in sheets)
+    //         {
+    //             var sheetId = sheet.Id;
+    //             var sheetName = sheet.Name;
+    //             
+    //             if (sheet.Sprites.Length <= 0) continue;
+    //             
+    //             
+    //         }
+    //     }
+    // }
 }
--- Assets/Scripts/SpriteSheetAsset.cs
+++ Assets/Scripts/SpriteSheetAsset.cs
@@ -6,14 +6,21 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "SpriteSheetAsset", menuName = "New Asset/SpriteSheetAsset")]
 public class SpriteSheetAsset : ScriptableObject
 {
-    public ushort id;
-    public Sheet[] Sheets;
-
-    [Serializable]
-    public class Sheet
-    {
-        public ushort ID;
-        public string Name;
-        public Sprite[] Sprites;
-    }
+    // [SerializeField] private ushort _id;
+    // [SerializeField] private SpriteSheet[] _spritesheets;
+    //
+    // public ushort Id => _id;
+    // public ReadOnlyMemory<SpriteSheet> SpriteSheets => _spritesheets;
+    //
+    // [Serializable]
+    // public class SpriteSheet
+    // {
+    //     [SerializeField] private ushort _id;
+    //     [SerializeField] private string _name;
+    //     [SerializeField] private Sprite[] _sprites;
+    //     
+    //     public ushort Id => _id;
+    //     public string Name => _name;
+    //     public ReadOnlyMemory<Sprite> Sprites => _sprites;
+    // }
 }
